7d0a094666ea6c4db5cf303a386a822421623e0b
MyPC <a>
2018-05-27 14:01:02 +0800

    数据库oauth

--- src/main/java/org/master/config/Config.java
+++ src/main/java/org/master/config/Config.java
@@ -5,24 +5,28 @@
  */
 package org.master.config;
 
-import java.util.Arrays;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 public class Config {
 
@@ -33,21 +37,50 @@ public class Config {
 
     @Configuration
     @EnableAuthorizationServer
+    @PropertySources({
+        @PropertySource("classpath:/sys.properties")
+    })
     static class OAuthAuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+
+        @Autowired
+        private Environment env;
 
         @Override
         public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-            clients.inMemory()
-                    .withClient(OAUTH_CLIENT_ID)
-                    .secret(OAUTH_CLIENT_SECRET)
-                    .resourceIds(RESOURCE_ID)
-                    .scopes(SCOPES)
-                    .authorities("ROLE_USER")
-                    .authorizedGrantTypes("authorization_code", "refresh_token")
-                    .redirectUris("http://www.baidu.com")
-                    .accessTokenValiditySeconds(60 * 30) // 30min  
-                    .refreshTokenValiditySeconds(60 * 60 * 24); // 24h  
+
+            clients.jdbc(dataSource());
+//                    .withClient(OAUTH_CLIENT_ID)
+//                    .secret(OAUTH_CLIENT_SECRET)
+//                    .resourceIds(RESOURCE_ID)
+//                    .scopes(SCOPES)
+//                    .authorities("ROLE_USER")
+//                    .authorizedGrantTypes("authorization_code", "refresh_token")
+//                    .redirectUris("http://www.baidu.com")
+//                    .accessTokenValiditySeconds(60 * 30) // 30min  
+//                    .refreshTokenValiditySeconds(60 * 60 * 24); // 24h  
         }
+
+        @Override
+        public void configure(
+                AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints
+                    .tokenStore(tokenStore());
+        }
+
+        @Bean
+        public TokenStore tokenStore() {
+            return new JdbcTokenStore(dataSource());
+        }
+
+        private DataSource dataSource() {
+            DriverManagerDataSource dataSource = new DriverManagerDataSource();
+            dataSource.setDriverClassName(env.getProperty("mysql.dirver"));
+            dataSource.setUrl(env.getProperty("mysql.jdbcUrl"));
+            dataSource.setUsername(env.getProperty("mysql.username"));
+            dataSource.setPassword(env.getProperty("mysql.password"));
+            return dataSource;
+        }
+
     }
 
     @Configuration
@@ -77,7 +110,7 @@ public class Config {
                     .withUser("user").password("123").roles("USER")
                     .and()
                     .withUser("admin").password("123").roles("ADMIN");
-             
+
         }
 
         @Override
